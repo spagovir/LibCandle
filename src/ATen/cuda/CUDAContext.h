723109134169a1507b9a504315fbf71481bf7657
Edgar <edgarlin2000@gmail.com>
2024-01-06 02:03:15 +0000

    Cuda Device Properties

--- src/ATen/cuda/CUDAContext.h
+++ src/ATen/cuda/CUDAContext.h
@@ -6,6 +6,23 @@
 #define C10_CUDA_CHECK(expr) libcandle::check(expr, __FILE__, __func__, static_cast<int>(__LINE__))
 #define C10_CUDA_KERNEL_LAUNCH_CHECK() C10_CUDA_CHECK(cudaGetLastError())
 
+namespace at::cuda
+{
+    cudaDeviceProp* getDeviceProperties(int64_t device)
+    {
+        cudaDeviceProp* prop = new cudaDeviceProp;
+        C10_CUDA_CHECK(cudaGetDeviceProperties(prop, device))
+        return prop;
+    }
+    cudaDeviceProp* getCurrentDeviceProperties()
+    {
+        int device;
+        c10_CUDA_CHECK(cudaGetDevice(&device));
+        return device;
+    }
+        
+}
+
 namespace libcandle 
 { 
     void check(cudaError_t err, char* file, char* function, int line)
